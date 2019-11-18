1d13e34450ac421cfbd2159d6bb03703b4b864e3
Boriss Melikjan <boris@avalanchelabs.ee>
2019-11-18 20:24:34 +0200

    Init with old repo. --no-videos

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template xmlns:v="http://www.w3.org/1999/xhtml">
+  <div id="app">
+    <img width="320" height="auto" src="./assets/star.gif">
+    <router-view/>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App'
+}
+
+</script>
+
+<style>
+  body {
+    background-color: black;
+    color: white;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: deeppink;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/assets/videos/ren.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($handle = opendir('.')) {
+    while (false !== ($fileName = readdir($handle))) {
+        $newName = md5($fileName) . '.mp4';
+        echo '{"hash":"'.$newName.'", "url": "'.$fileName.'"}';
+        rename($fileName, $newName);
+        if ($handleThumbs = opendir('../thumbs/')) {
+            while (false !== ($fileNameThumbs = readdir($handleThumbs))) {
+                $newName = '../thumbs/' .  md5($fileName) . '.jpg';
+                rename('../thumbs/' .  $fileNameThumbs, $newName);
+            }
+        }
+    }
+    closedir($handle);
+}
+?>
